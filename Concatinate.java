85bc7f7d3f841911ffc2796f7d655ade8e5a6357
agyofficials <80948489+agyofficials@users.noreply.github.com>
2025-02-12 21:22:06 +0530

    Concatinate.java

---
+++ Concatinate.java
@@ -0,0 +1,39 @@
+public class Concate {
+    public static void main(String[] args) {
+        //String Concatinate
+
+        //How to design a varibale name
+        // casing technique
+        // TitleCase = Firstname
+        // CapitalEachWord = LastName
+        // UPPERCASE = FIRSTNAME
+        // lowercase = lastname
+        // camelcase = firstName, firstNAME, FIRSTname
+
+        //***Do and don't of a varible
+
+        // Dont's
+        // 1. Cannot Start with the number example 123Hello - WRONG
+        // 2. Cannot have any special character example - Hello@World _ WRONG
+        // 3. Cannot have a space in bitween example First Name - WRONG
+
+        // Do's
+        //Can have number but needs to start with letter example - H12345
+        // int H12345 = 10; - Correct
+        // int 123Hello = 10; - Inccorect
+
+        //Can only have 1 Special Char named as underscore "_"
+        // int variable name = 10; - Incorrect
+        // int variable_name = 10; - Correct
+
+
+
+
+        
+        String stringOne = "Anurag";
+        String stringTwo = "Garg";
+        int value = 10;
+
+        System.out.println(stringOne + stringTwo + value);
+    }
+}
