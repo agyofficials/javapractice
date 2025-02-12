956c208114331648363953604006ded0ba9d7228
agyofficials <80948489+agyofficials@users.noreply.github.com>
2025-02-12 21:04:00 +0530

    Add files via upload

---
+++ Array_new.java
@@ -0,0 +1,35 @@
+public class Array_new {
+    public static void main(String[] args) {
+    //1st lecture on Arrays
+    //Let's Start an Object Oriented Programming
+    //One Dimensional Array
+
+    // data_type variable_name = value;
+    // array_data_type [] name_of_the_array = new value_type[size];
+
+
+
+    // formula of index = size - 1;
+    // index = 5 - 1
+    // index = 4
+
+    int variable = 10; //this is how we decleare a variable
+
+    int [] array = new int[5]; //this is how we initialise a array in Java
+
+    // index   0,  1,  2,  3,  4
+    // element 11, 22, 33, 44, 55
+
+    array[0] = 11; //this is how we assign the element on the index of the array
+    array[1] = 22;
+    array[2] = 33;
+    array[3] = 44;
+    array[4] = 55;
+
+    System.out.println("This is a variable " + variable); //this is how we call the value of a variable
+    System.out.println("This is an array " + array[4]);
+
+
+
+    }
+}
